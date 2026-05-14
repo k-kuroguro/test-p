9d0302f82e672b8130ff8c22f264b0c7b42b8061
k-kuroguro <68765710+k-kuroguro@users.noreply.github.com>
2026-05-14 23:15:38 +0900

    a

--- tests/test.sh
+++ tests/test.sh
@@ -4,5 +4,4 @@ set -eu
 
 output="$(craftos --headless --script tests/runner.lua --mount .=tests/ 2>/dev/null)"
 
-printf '%s\n' "$output"
 printf '%s\n' "$output" | grep -Fq "[PASS] Generated test completed successfully."
