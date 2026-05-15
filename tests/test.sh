e2ea944651ba6723fb6e7f749e503c1b16b1df26
k-kuroguro <68765710+k-kuroguro@users.noreply.github.com>
2026-05-16 01:34:02 +0900

    a

--- tests/test.sh
+++ tests/test.sh
@@ -2,8 +2,14 @@
 
 set -eu
 
-output="$(craftos --headless --script tests/runner.lua --mount .=tests/ 2>/dev/null)"
-output="$(printf '%s\n' "${output}" | sed -n '/======== START ========/,$p' | tail -n +2)"
+tmp="$(mktemp)"
+craftos \
+  --headless \
+  --script tests/runner.lua \
+  --mount .=tests/ \
+  > "${tmp}" 2>/dev/null
+
+output="$(sed -n '/======== START ========/,$p' "${tmp}" | tail -n +2)"
 
 printf '%s\n' "${output}"
 printf '%s\n' "${output}" | grep -Fq "[PASS] Generated test finished successfully."
